a9e10076b1f7a3bbe30399a2752fdaa1913b1d3c
ArtixZ <artixzhu@gmail.com>
2017-10-20 09:05:40 -0400

    nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,36 @@ import React, { Component } from 'react';
 export default class Header extends Component {
   render() {
     return (
-      <div className="app-header flex-row justify-center align-center">
+      <div>
+        <div className="app-header flex-row justify-center align-center">
           <div style={{flex: 1}} />      
           <div style={{flex: 1}} className="flex-row justify-center align-center">
             <span className="title">THE TIMES</span>
           </div>
-          <div style={{flex: 1}}>          
-            <input className="" type="text" name="search" placeholder="Search" />
+          <div style={{flex: 1}} className="flex-row justify-end">          
+            <input className="search-box" type="text" name="search" placeholder="Search" />
           </div>
+        </div>
+        <div className="nav-bar flex-row justify-center align-center">
+          <div style={{flex: 1}} className="flex-row justify-center align-center">
+            <a href="#home">Home</a>
+          </div>
+          <div style={{flex: 1}} className="flex-row justify-center align-center">
+            <a >Home</a>
+          </div>
+          <div style={{flex: 1}} className="flex-row justify-center align-center">
+            <a>Home</a>
+          </div>
+          <div style={{flex: 1}} className="flex-row justify-center align-center">
+            <a>Home</a>
+          </div>
+          <div style={{flex: 1}} className="flex-row justify-center align-center">
+            <a>Home</a>
+          </div>
+          <div style={{flex: 1}} className="flex-row justify-center align-center">
+            <a>Home</a>
+          </div>
+        </div>
       </div>
     );
   }
