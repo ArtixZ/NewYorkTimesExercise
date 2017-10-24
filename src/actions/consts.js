5d8f3f39ce1fc90c3e3cd517fbe0866e840a34e5
ArtixZ <artixzhu@gmail.com>
2017-10-24 02:42:41 -0400

    create actions

--- src/actions/consts.js
+++ src/actions/consts.js
@@ -1,1 +1,3 @@
 export const TOP_STORY = "TOP_STORY";
+export const ARCHIVE_STORY = "ARCHIVE_STORY";
+export const GET_SEARCH = "GET_SEARCH";
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,16 @@
 import {
-    TOP_STORY
+    TOP_STORY,
+    ARCHIVE_STORY,
+    GET_SEARCH
 } from './consts';
+import moment from 'moment';
 
+const curYear = moment().year();
+const curMonth = moment().month()+1;
 const APIKey = "a8457610b68381085a3fff38d6a36337:6:74255139";
 const topStoryAPI = "https://api.nytimes.com/svc/topstories/v2/home.json";
-const archiveAPI = "https://api.nytimes.com/svc/archive/v1/2017/10.json";
+const archiveAPI = `https://api.nytimes.com/svc/archive/v1/${curYear}/${curMonth}.json`;
+const searchAPI = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 
 const callAPI = (method, url, params) => {
 
@@ -33,6 +39,8 @@ const callAPI = (method, url, params) => {
     //     return res.json()
     // })
 }
+
+// home actions
 const dispathTopStory = (payload) => {
     return {
         type: TOP_STORY,
@@ -42,8 +50,45 @@ const dispathTopStory = (payload) => {
 export const getTopStory = () => {
     return( dispatch, getState )=> {
         callAPI('GET', topStoryAPI).then( res =>{
-            dispatch(dispathTopStory(res.results))
+            dispatch(dispathTopStory(res.results));
         })
     }
 }
 
+const dispatchArchiveStory = (payload) => {
+    return {
+        type: ARCHIVE_STORY,
+        payload
+    }
+}
+
+export const getArchiveStory = () => {
+    return( dispatch, getState ) => {
+        callAPI('GET', archiveAPI).then( res => {
+            dispatch(dispatchArchiveStory(res.response.docs));
+        })
+    }
+}
+
+// search actions
+
+const dispatchSearchRes = (payload) => {
+    return {
+        type: GET_SEARCH,
+        payload
+    }
+}
+export const getSearchRes = (searchVal) => {
+    return(dispatch, getState) => {
+        callAPI('GET', searchAPI, {
+            q: searchVal
+        }).then( res => {
+            dispatch(dispatchSearchRes(res.response.docs));
+        })
+    }
+}
+export const clearSearchRes = () => {
+    return(dispatch) => {
+        dispatch(dispatchSearchRes(null));
+    }
+}
