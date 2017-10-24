df83fd5e74dc105c188cce83ea003a5b45f4a438
ArtixZ <artixzhu@gmail.com>
2017-10-24 02:43:03 -0400

    Create home page reducers

--- src/reducers/HomePageReducers.js
+++ src/reducers/HomePageReducers.js
@@ -1,5 +1,8 @@
+import moment from 'moment';
+
 import {
-    TOP_STORY
+    TOP_STORY,
+    ARCHIVE_STORY,
 } from '../actions/consts';
 
 
@@ -13,6 +16,15 @@ export default function(state = INITIAL_STATE, action) {
     switch(action.type) {
         case TOP_STORY:
             return {...state, topStory: action.payload, topStoryLoading: false}
+        case ARCHIVE_STORY:
+            const weekStory = [];
+            for(let i=action.payload.length-1;i>0;i--) {
+                if(moment(action.payload[i].pub_date).isBefore(moment().subtract(7,'d'))) break;
+                if(action.payload[i].multimedia.length>0){
+                    weekStory.push(action.payload[i]);
+                }
+            }
+            return {...state, archiveStory: weekStory, archiveStoryLoading: false}
         default: 
             return state;
     }
