8e2899806f64cf3b4fd0f21130f9a0b1e3095a4e
ArtixZ <artixzhu@gmail.com>
2017-10-24 02:43:30 -0400

    create Header and search input

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class Header extends Component {
+import { getSearchRes, clearSearchRes } from '../actions'
+import SearchPage from './SearchPage';
+
+class Header extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state={
+      searchVal: ''
+    }
+  }
+  
+  onSearchChange = (e) => {
+    this.setState({
+      searchVal: e.target.value
+    })
+  }
+
+  handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      const { getSearchRes } = this.props;
+      getSearchRes(this.state.searchVal);      
+    }
+  }
+
+  goOutFromSearch = () => {
+    const { clearSearchRes } = this.props;
+    clearSearchRes();
+  }
+
   render() {
+    console.log(this.props.searchRes, this.props.location)
+    const { searchRes, location } = this.props;
     return (
       <div>
         <div className="app-header flex-row justify-center align-center">
@@ -10,31 +42,46 @@ export default class Header extends Component {
             <span className="title">THE TIMES</span>
           </div>
           <div style={{flex: 1}} className="flex-row justify-end">          
-            <input className="search-box" type="text" name="search" placeholder="Search" />
+            <input className="search-box" type="text" name="search" placeholder="Search" value={this.state.searchVal} onChange={this.onSearchChange} onKeyPress={this.handleKeyPress}/>
           </div>
         </div>
         <div className="nav-bar flex-row justify-center align-center paddingLeftRight">
           <div style={{flex: 1}} className="max-height flex-row justify-center align-center active">
-            <div>Home</div>
+            <div className="tag">Home</div>
           </div>
           <div style={{flex: 1}} className="max-height flex-row justify-center align-center">
-            <a >World</a>
+            <div className="tag">World</div>
           </div>
           <div style={{flex: 1}} className="max-height flex-row justify-center align-center">
-            <a>U.S.</a>
+            <div className="tag">U.S.</div>
           </div>
           <div style={{flex: 1}} className="max-height flex-row justify-center align-center">
-            <a>Politics</a>
+            <div className="tag">Politics</div>
           </div>
           <div style={{flex: 1}} className="max-height flex-row justify-center align-center">
-            <a>N.Y.</a>
+            <div className="tag">N.Y.</div>
           </div>
           <div style={{flex: 1}} className="max-height flex-row justify-center align-center">
-            <a>More</a>
+            <div className="tag">More</div>
           </div>
         </div>
-        {this.props.children}
+        { searchRes ?
+          <SearchPage 
+            location={location.pathname}
+            goBack={this.goOutFromSearch}
+            searchRes={searchRes}
+          />
+          : this.props.children
+        }
       </div>
     );
   }
 }
+const mapStateToProps = (state) => {
+  return {searchRes: state.searchRes}
+};
+
+export default connect(mapStateToProps, { 
+  getSearchRes,
+  clearSearchRes
+})(Header);
